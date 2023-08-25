545f5353541271b5bca102fc256288c73575ec92
Crowbyte Games <crowbytegames@gmail.com>
2023-08-25 00:46:31 -0400

    Unity Template Scripts and Project

--- Unity Template/Assets/Scripts/Pointer.cs
+++ Unity Template/Assets/Scripts/Pointer.cs
@@ -45,6 +45,10 @@ public class Pointer : MonoBehaviour
             {
                 Ray.transform.parent.transform.localEulerAngles = new Vector3(-deviation.y, deviation.x, 0);
             }
+            else
+            {
+                Ray.transform.parent.transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
         }
         else
         {
@@ -52,6 +56,7 @@ public class Pointer : MonoBehaviour
             PointerDot.transform.position = Player.Camera.transform.position;
             PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x + -deviation.y, Player.Camera.transform.eulerAngles.y + deviation.x, 0);
             PointerDot.transform.Translate(new Vector3(0, 0, rayMaxDistance));
+
             pointerPosition = PointerDot.transform.position;
 
             // Ray Distance
@@ -62,9 +67,94 @@ public class Pointer : MonoBehaviour
             {
                 Ray.transform.parent.transform.localEulerAngles = new Vector3(-deviation.y, deviation.x, 0);
             }
+            else
+            {
+                Ray.transform.parent.transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
         }
 
         // Move Pointer Dot
         PointerDot.transform.position = pointerPosition;
+    }
+
+    // Singular Shot
+    public RaycastHit Shoot(float maxDistance, bool randomDeviation, Vector2 deviation, out Vector3 hitPosition, out Collider collision)
+    {
+        hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2 + deviation.y, 0));
+
+        // Deviation
+        if (randomDeviation)
+        {
+            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
+        }
+        else
+        {
+            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
+        }
+
+        // Check Hitscan
+        if (Physics.Raycast(hitscan, out hitpoint, maxDistance))
+        {
+            // Calculate Pointer Hit Position and Collision
+            hitPosition = hitpoint.point;
+            collision = hitpoint.collider;
+        }
+        else
+        {
+            // Calculate Position
+            PointerDot.transform.position = Player.Camera.transform.position;
+            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x + -deviation.y, Player.Camera.transform.eulerAngles.y + deviation.x, 0);
+            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
+
+            hitPosition = PointerDot.transform.position;
+            collision = null;
+        }
+
+        return hitpoint;
+    }
+
+    // Multiple Shots at Once
+    public RaycastHit[] ShootBurst(int totalShots, float maxDistance, bool randomDeviation, Vector2 deviation, out Vector3[] hitPositions, out Collider[] collisions)
+    {
+        RaycastHit[] hitpoints = new RaycastHit[totalShots];
+        hitPositions = new Vector3[totalShots];
+        collisions = new Collider[totalShots];
+
+        for (int shotIndex = 0; shotIndex < totalShots; shotIndex++)
+        {
+            hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2 + deviation.y, 0));
+
+            // Deviation
+            if (randomDeviation)
+            {
+                hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
+            }
+            else
+            {
+                hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
+            }
+
+            // Check Hitscan
+            if (Physics.Raycast(hitscan, out hitpoint, maxDistance))
+            {
+                // Calculate Pointer Hit Position and Collision
+                hitPositions[shotIndex] = hitpoint.point;
+                collisions[shotIndex] = hitpoint.collider;
+            }
+            else
+            {
+                // Calculate Position
+                PointerDot.transform.position = Player.Camera.transform.position;
+                PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x + -deviation.y, Player.Camera.transform.eulerAngles.y + deviation.x, 0);
+                PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
+
+                hitPositions[shotIndex] = PointerDot.transform.position;
+                collisions[shotIndex] = null;
+            }
+
+            hitpoints[shotIndex] = hitpoint;
+        }
+
+        return hitpoints;
     }
 }
