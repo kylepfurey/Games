f69bc37b944581a124567aaa7340600d45d55b57
Crowbyte Games <crowbytegames@gmail.com>
2023-08-26 13:14:57 -0400

    Unity Template Scripts and Project

--- Unity Template/Assets/Scripts/Pointer.cs
+++ Unity Template/Assets/Scripts/Pointer.cs
@@ -8,11 +8,12 @@ public class Pointer : MonoBehaviour
 
     public Ray hitscan;
     public RaycastHit hitpoint;
+    public Ray hitscanCamera;
+    public RaycastHit hitpointCamera;
     public float maxDistance;
-    public float currentDistance;
 
     public Vector3 pointerPosition;
-    public Vector3 pointerCenterPosition;
+    public Vector3 pointerCameraPosition;
 
     public bool randomDeviation;
     public bool fixedDeviation;
@@ -23,12 +24,37 @@ public class Pointer : MonoBehaviour
 
     void Update()
     {
-        hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2 + deviation.y, 0));
+        // Calculate Center Position from Camera
+        hitscanCamera = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2, 0));
+
+        if (Physics.Raycast(hitscanCamera, out hitpointCamera, maxDistance))
+        {
+            pointerCameraPosition = hitpointCamera.point;
+        }
+        else
+        {
+            PointerDot.transform.position = Player.Camera.transform.position;
+            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x, Player.Camera.transform.eulerAngles.y, 0);
+            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
+            pointerCameraPosition = PointerDot.transform.position;
+        }
+
+
+        // Hitscan
+        Vector3 currentPosition = Player.Camera.transform.position;
+        Vector3 currentRotation = Player.Camera.transform.eulerAngles;
+        Player.Camera.transform.position = Player.transform.position + new Vector3(0, 0, 0);
+        Player.Camera.transform.LookAt(pointerCameraPosition);
+        Vector3 aimRotation = Player.Camera.transform.eulerAngles;
+        hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2, 0));
+        Player.Camera.transform.position = currentPosition;
+        Player.Camera.transform.eulerAngles = currentRotation;
+
 
         // Deviation
         if (randomDeviation)
         {
-            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
+            hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
         }
         else if (fixedDeviation)
         {
@@ -130,66 +156,38 @@ public class Pointer : MonoBehaviour
                 shotCounter = 0;
             }
 
-            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-deviationY, deviationX, 0)) * Vector3.forward;
+            hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-deviationY, deviationX, 0)) * Vector3.forward;
         }
         else
         {
-            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
+            hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
         }
+
 
         // Check Hitscan
         if (Physics.Raycast(hitscan, out hitpoint, maxDistance))
         {
-            // Calculate Center Position
-            PointerDot.transform.position = Player.Camera.transform.position;
-            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x, Player.Camera.transform.eulerAngles.y, 0);
-            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
-            pointerCenterPosition = PointerDot.transform.position;
-
-            // Calculate Pointer Hit Position
+            // Calculate Hit Position
             pointerPosition = hitpoint.point;
 
-            // Ray Distance
-            currentDistance = Mathf.Abs(Vector3.Distance(Player.Camera.transform.position, pointerPosition));
-            Ray.transform.localScale = new Vector3(Ray.transform.localScale.x, Ray.transform.localScale.y, currentDistance);
-            Ray.transform.localPosition = new Vector3(0, 0, currentDistance / 2);
-
-            if (randomDeviation == false && fixedDeviation == false)
-            {
-                Ray.transform.parent.transform.localEulerAngles = new Vector3(-deviation.y, deviation.x, 0);
-            }
-            else
-            {
-                Ray.transform.parent.transform.localEulerAngles = new Vector3(0, 0, 0);
-            }
+            // Render Ray
+            Ray.transform.parent.position = hitscan.origin;
+            Ray.transform.parent.transform.LookAt(pointerCameraPosition);
+            Ray.transform.localScale = new Vector3(Ray.transform.localScale.x, Ray.transform.localScale.y, hitpoint.distance);
+            Ray.transform.localPosition = new Vector3(0, 0, hitpoint.distance / 2);
         }
         else
         {
-            // Calculate Center Position
-            PointerDot.transform.position = Player.Camera.transform.position;
-            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x, Player.Camera.transform.eulerAngles.y, 0);
-            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
-            pointerCenterPosition = PointerDot.transform.position;
+            // Calculate End Position
+            pointerPosition = pointerCameraPosition;
 
-            // Calculate Position
-            PointerDot.transform.position = Player.Camera.transform.position;
-            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x + -deviation.y, Player.Camera.transform.eulerAngles.y + deviation.x, 0);
-            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
-            pointerPosition = PointerDot.transform.position;
-
-            // Ray Distance
+            // Render Ray
+            Ray.transform.parent.position = hitscan.origin;
+            Ray.transform.parent.transform.LookAt(pointerCameraPosition);
             Ray.transform.localScale = new Vector3(Ray.transform.localScale.x, Ray.transform.localScale.y, maxDistance);
-            Ray.transform.localPosition = new Vector3(0, 0, maxDistance / 2);
-
-            if (randomDeviation == false && fixedDeviation == false)
-            {
-                Ray.transform.parent.transform.localEulerAngles = new Vector3(-deviation.y, deviation.x, 0);
-            }
-            else
-            {
-                Ray.transform.parent.transform.localEulerAngles = new Vector3(0, 0, 0);
-            }
+            Ray.transform.localPosition = new Vector3(0, 0, maxDistance / 2 - Player.Camera.nearClipPlane);
         }
+
 
         // Move Pointer Dot
         PointerDot.transform.position = pointerPosition;
@@ -198,36 +196,62 @@ public class Pointer : MonoBehaviour
     // Singular Shot
     public RaycastHit Shoot(float maxDistance, bool randomDeviation, Vector2 deviation, out Vector3 hitPosition, out Collider collision)
     {
-        hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2 + deviation.y, 0));
+        // Calculate Center Position from Camera
+        hitscanCamera = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2, 0));
+
+        if (Physics.Raycast(hitscanCamera, out hitpointCamera, maxDistance))
+        {
+            pointerCameraPosition = hitpointCamera.point;
+        }
+        else
+        {
+            PointerDot.transform.position = Player.Camera.transform.position;
+            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x, Player.Camera.transform.eulerAngles.y, 0);
+            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
+            pointerCameraPosition = PointerDot.transform.position;
+        }
+
+
+        // Hitscan
+        Vector3 currentPosition = Player.Camera.transform.position;
+        Vector3 currentRotation = Player.Camera.transform.eulerAngles;
+        Player.Camera.transform.position = Player.transform.position + new Vector3(0, 0, 0);
+        Player.Camera.transform.LookAt(pointerCameraPosition);
+        Vector3 aimRotation = Player.Camera.transform.eulerAngles;
+        hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2, 0));
+        Player.Camera.transform.position = currentPosition;
+        Player.Camera.transform.eulerAngles = currentRotation;
+
 
         // Deviation
         if (randomDeviation)
         {
-            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
+            hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
         }
         else
         {
-            hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
+            hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
         }
+
 
         // Check Hitscan
         if (Physics.Raycast(hitscan, out hitpoint, maxDistance))
         {
-            // Calculate Pointer Hit Position and Collision
+            // Calculate Hit Position
+            pointerPosition = hitpoint.point;
             hitPosition = hitpoint.point;
             collision = hitpoint.collider;
         }
         else
         {
-            // Calculate Position
-            PointerDot.transform.position = Player.Camera.transform.position;
-            PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x + -deviation.y, Player.Camera.transform.eulerAngles.y + deviation.x, 0);
-            PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
-
-            hitPosition = PointerDot.transform.position;
+            // Calculate End Position
+            pointerPosition = pointerCameraPosition;
+            hitPosition = pointerCameraPosition;
             collision = null;
         }
 
+
+        // Return Hit Data
         return hitpoint;
     }
 
@@ -238,21 +262,45 @@ public class Pointer : MonoBehaviour
         hitPositions = new Vector3[totalShots];
         collisions = new Collider[totalShots];
 
-        int shotCounter = 0;
-        float deviationX = 0;
-        float deviationY = 0;
-
-        for (int shotIndex = 0; shotIndex < totalShots; shotIndex++)
+        for (int currentShot = 0; currentShot < totalShots; currentShot++)
         {
-            hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2 + deviation.y, 0));
+            // Calculate Center Position from Camera
+            hitscanCamera = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2, 0));
+
+            if (Physics.Raycast(hitscanCamera, out hitpointCamera, maxDistance))
+            {
+                pointerCameraPosition = hitpointCamera.point;
+            }
+            else
+            {
+                PointerDot.transform.position = Player.Camera.transform.position;
+                PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x, Player.Camera.transform.eulerAngles.y, 0);
+                PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
+                pointerCameraPosition = PointerDot.transform.position;
+            }
+
+
+            // Hitscan
+            Vector3 currentPosition = Player.Camera.transform.position;
+            Vector3 currentRotation = Player.Camera.transform.eulerAngles;
+            Player.Camera.transform.position = Player.transform.position + new Vector3(0, 0, 0);
+            Player.Camera.transform.LookAt(pointerCameraPosition);
+            Vector3 aimRotation = Player.Camera.transform.eulerAngles;
+            hitscan = Player.Camera.ScreenPointToRay(new Vector3(Player.Camera.pixelWidth / 2, Player.Camera.pixelHeight / 2, 0));
+            Player.Camera.transform.position = currentPosition;
+            Player.Camera.transform.eulerAngles = currentRotation;
+
 
             // Deviation
             if (randomDeviation)
             {
-                hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
+                hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-Random.Range(-deviation.y, deviation.y), Random.Range(-deviation.x, deviation.x), 0)) * Vector3.forward;
             }
-            else
+            else if (fixedDeviation)
             {
+                float deviationX = 0;
+                float deviationY = 0;
+
                 // Bullet Pattern
                 switch (shotCounter)
                 {
@@ -343,28 +391,34 @@ public class Pointer : MonoBehaviour
                         break;
                 }
 
-                hitscan.direction = Quaternion.Euler(Player.Camera.transform.eulerAngles + new Vector3(-deviationY, deviationX, 0)) * Vector3.forward;
+                if (shotCounter > totalShots - 1)
+                {
+                    shotCounter = 0;
+                }
+
+                hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-deviationY, deviationX, 0)) * Vector3.forward;
             }
+            else
+            {
+                hitscan.direction = Quaternion.Euler(aimRotation + new Vector3(-deviation.y, deviation.x, 0)) * Vector3.forward;
+            }
+
 
             // Check Hitscan
             if (Physics.Raycast(hitscan, out hitpoint, maxDistance))
             {
-                // Calculate Pointer Hit Position and Collision
-                hitPositions[shotIndex] = hitpoint.point;
-                collisions[shotIndex] = hitpoint.collider;
+                // Calculate Hit Position
+                pointerPosition = hitpoint.point;
+                hitPositions[currentShot] = hitpoint.point;
+                collisions[currentShot] = hitpoint.collider;
             }
             else
             {
-                // Calculate Position
-                PointerDot.transform.position = Player.Camera.transform.position;
-                PointerDot.transform.eulerAngles = new Vector3(Player.Camera.transform.eulerAngles.x + -deviation.y, Player.Camera.transform.eulerAngles.y + deviation.x, 0);
-                PointerDot.transform.Translate(new Vector3(0, 0, maxDistance));
-
-                hitPositions[shotIndex] = PointerDot.transform.position;
-                collisions[shotIndex] = null;
+                // Calculate End Position
+                pointerPosition = pointerCameraPosition;
+                hitPositions[currentShot] = pointerCameraPosition;
+                collisions[currentShot] = null;
             }
-
-            hitpoints[shotIndex] = hitpoint;
         }
 
         return hitpoints;
