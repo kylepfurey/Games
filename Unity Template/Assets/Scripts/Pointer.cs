0dcfa4923567891f96a832a09c2fecdb827ffcac
Crowbyte Games <crowbytegames@gmail.com>
2023-08-27 20:14:33 -0400

    Unity Template Scripts and Project

--- Unity Template/Assets/Scripts/Pointer.cs
+++ Unity Template/Assets/Scripts/Pointer.cs
@@ -385,10 +385,8 @@ public class Pointer : MonoBehaviour
             // Move Pointer Dot
             PointerDot.transform.position = pointerPosition;
         }
-    }
 
-    void FixedUpdate()
-    {
+
         // Face Player
         Vector3 playerRotation = Player.transform.eulerAngles;
         Player.transform.LookAt(PointerDot.transform.position);
